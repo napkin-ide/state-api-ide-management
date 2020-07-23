72c69aadb228dc4014aaddba01dad3154d62703d
todd.german <todd.german@fathym.com>
2020-07-22 18:54:30 -0600

    added tests project, stubbed tests

---
+++ state-api-ide-management-tests/Settings/SaveLCUTests.cs
@@ -0,0 +1,43 @@
+using Fathym.Testing;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace state_api_ide_management_tests
+{
+    [TestClass]
+    public class SaveLCUTests : AzFunctionTestBase
+    {
+        
+        public SaveLCUTests() : base()
+        {
+            APIRoute = "api/SaveLCU";                
+        }
+
+        [TestMethod]
+        public async Task TestSaveLCU()
+        {
+            LcuEntApiKey = "";            
+            PrincipalId = "";
+
+            addRequestHeaders();
+
+            var url = $"{HostURL}/{APIRoute}";            
+
+            var response = await httpGet(url); 
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            var model = getContent<dynamic>(response);
+
+            dynamic result = model.Result;            
+
+            throw new NotImplementedException("Implement me!");                  
+        }
+    }
+}
